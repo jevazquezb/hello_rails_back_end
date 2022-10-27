ed2f86377b9cd2bd0580bc4378e72ca69de75522
jevazquezb <je.vazquezb@gmail.com>
2022-10-26 19:56:38 -0500

    Add API routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,11 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  namespace :api, defaults: { format: :json } do
+    namespace :v1 do
+      # get 'greetings/index'
+      resources :greetings, only: :index
+    end
+  end
 end
